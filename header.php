4d4f33596a019aefa2410aa0035b7dc4739e7ae9
lasy24935 <lasy24935@gmail.com>
2020-05-27 00:29:25 +0200

    update

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 </head>
   <body>
       
-       <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: purple;">
+       <nav class="navbar navbar-expand-sm navbar-dark navbar-fixed-top" style="background-color: purple;">
            <a class="navbar-brand" href="index.php">Adresse</a>
            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                aria-expanded="false" aria-label="Toggle navigation"></button>
